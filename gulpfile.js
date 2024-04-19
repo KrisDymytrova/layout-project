c5e89e0c2a9956d4f5f559af7b4cdfdab0f53ba6
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-04-19 23:46:29 +0300

    HW 75 Completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,35 @@
+'use strict';
 
+const gulp = require('gulp');
+const { series, parallel } = require('gulp');
+
+function compileSass(cb) {
+    console.log('Compiling SASS...');
+    cb();
+}
+
+function minifyJS(cb) {
+    console.log('Minifying JavaScript...');
+    cb();
+}
+
+function optimizeImages(cb) {
+    console.log('Optimizing images...');
+    cb();
+}
+
+function cleanTempFiles(cb) {
+    console.log('Cleaning temporary files...');
+    cb();
+}
+
+exports.compileSass = compileSass;
+exports.minifyJS = minifyJS;
+exports.optimizeImages = optimizeImages;
+exports.cleanTempFiles = cleanTempFiles;
+
+exports.build = parallel(compileSass, minifyJS);
+
+exports.production = series(exports.build, optimizeImages);
+
+exports.default = series(exports.build, cleanTempFiles);
