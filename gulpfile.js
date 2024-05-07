7d625b572db09a25b532dc315b61ecc5311d4c8b
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-05-07 03:32:24 +0300

    HW 76 Completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,32 @@
+'use strict';
+
+const { watch } = require('gulp');
+
+const changeHtmlFile = (done) => {
+    console.log('Изменился HTML файл');
+
+    done();
+};
+
+const fileAddition = (done) => {
+    console.log('Был добавлен новый файл');
+
+    done();
+};
+
+const fileDeletion = (done) => {
+    console.log(`Файл был удалён`);
+
+    done();
+};
+
+const watchers = () => {
+    watch('dist/pages/**/*.html', changeHtmlFile);
+
+    watch('dist/**/*', { events: 'add' }, fileAddition);
+
+    watch(['dist/js/', 'dist/styles/'], { events: 'unlink' }, fileDeletion);
+};
+
+exports.watchers = watchers;
 
