d0f6862b63aac8b5449efe51db9898082d285f64
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-05-08 03:13:27 +0300

    HW 77 Completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,33 @@
+'use strict'
 
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const pug = require('gulp-pug');
+const browserSync = require('browser-sync').create();
+
+gulp.task('sass', () => {
+    return gulp.src('dist/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/styles'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('pug', () => {
+    return gulp.src('dist/views/*.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('server', () => {
+    browserSync.init({
+        server: {
+            baseDir: './dist'
+        }
+    });
+
+    gulp.watch('dist/scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('dist/views/**/*.pug', gulp.series('pug'));
+});
+
+gulp.task('development', gulp.series('sass', 'pug', 'server'));
